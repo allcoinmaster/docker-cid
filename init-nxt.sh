aea6bb6f1818882ce74716c589c834d6ddf25d8b
Chain ID Project <37491678+chainidio@users.noreply.github.com>
2018-06-17 14:07:39 +0700

    Update init-nxt.sh

--- init-nxt.sh
+++ init-nxt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ ! -f "/nxt/.init" ]; then 
-	echo -e " init-nxt.sh: Performing init..."
+if [ ! -f "/cid/.init" ]; then 
+	echo -e " init-cid.sh: Performing init..."
 
 	# If there is no .init, this can be a new install
 	# or an upgrade... in the second case, we want to do some cleanup to ensure
@@ -14,9 +14,9 @@ if [ ! -f "/nxt/.init" ]; then
 	# then we run it before anything else starts
 	if [ -n "${SCRIPT-}" ]; then
 		filename=$(basename "$SCRIPT")
-		wget "$SCRIPT" -O "/nxt-boot/scripts/$filename"
-		chmod u+x "/nxt-boot/scripts/$filename"
-		/nxt-boot/scripts/$filename
+		wget "$SCRIPT" -O "/cid-boot/scripts/$filename"
+		chmod u+x "/cid-boot/scripts/$filename"
+		/cid-boot/scripts/$filename
 	fi  
 
 	cd /
@@ -29,7 +29,7 @@ if [ ! -f "/nxt/.init" ]; then
 	rm -Rf *.exe src changelogs
 
 	if [ -n "${PLUGINS-}" ]; then
-		/nxt-boot/scripts/install-plugins.sh "$PLUGINS"
+		/cid-boot/scripts/install-plugins.sh "$PLUGINS"
 	else
 		echo " PLUGINS not provided"
 	fi  
@@ -50,32 +50,32 @@ if [ ! -f "/nxt/.init" ]; then
 	if [ -n "${BLOCKCHAINDL-}" ] && [ ! -d "$DB" ]; then
 		echo " init-nxt.sh: $DB not found, downloading blockchain from $BLOCKCHAINDL";
 		wget "$BLOCKCHAINDL" && unzip *.zip && rm *.zip
-		echo " init-nxt.sh: Blockchain download complete"
+		echo " init-cid.sh: Blockchain download complete"
 	else
 		echo " BLOCKCHAINDL not provided"
 	fi
 
 	# linking of the config
 	if [ "$NXTNET" = "main" ]; then
-		echo " init-nxt.sh: Linking config to mainnet"
-		cp /nxt-boot/conf/nxt-main.properties /cid/conf/cid.properties
+		echo " init-cid.sh: Linking config to mainnet"
+		cp /cid-boot/conf/cid-main.properties /cid/conf/cid.properties
 	else
 		echo " init-nxt.sh: Linking config to testnet"
-		cp /nxt-boot/conf/nxt-test.properties /cid/conf/cid.properties
+		cp /cid-boot/conf/cid-test.properties /cid/conf/cid.properties
 	fi  
 
 	# if the admin password is defined in the ENV variable, we append to the config
 	if [ -n "${ADMINPASSWD-}" ]; then
-		echo -e "\nxt.adminPassword=${ADMINPASSWD-}" >> /cid/conf/cid.properties
+		echo -e "\cid.adminPassword=${ADMINPASSWD-}" >> /cid/conf/cid.properties
 	else
 		echo " ADMINPASSWD not provided"
 	fi
 
 	# If we did all of that, we dump a file that will signal next time that we
 	# should not run the init-script again
-	touch /nxt/.init
+	touch /cid/.init
 else
-	echo -e " init-nxt.sh: Init already done, skipping init."
+	echo -e " init-cid.sh: Init already done, skipping init."
 fi
 
 cd /ChainPlatform
