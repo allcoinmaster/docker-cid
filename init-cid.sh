0192d8d1b461f2ff2951c6799fdf288ec01f5c3e
Chain ID Project <37491678+chainidio@users.noreply.github.com>
2018-08-09 14:55:04 +0700

    Update init-cid.sh

--- init-cid.sh
+++ init-cid.sh
@@ -7,38 +7,38 @@ if [ ! -f "/cid/.init" ]; then
 	# or an upgrade... in the second case, we want to do some cleanup to ensure
 	# that the upgrade will go smooth
 	
-	rm -Rf /ChainPlatform/lib && \
-	mkdir /ChainPlatform/conf
+	rm -Rf /ChainCoin/lib && \
+	mkdir /ChainCoin/conf
 	
 	# if a script was provided, we download it locally
 	# then we run it before anything else starts
 	if [ -n "${SCRIPT-}" ]; then
 		filename=$(basename "$SCRIPT")
-		wget "$SCRIPT" -O "/cid-boot/scripts/$filename"
-		chmod u+x "/cid-boot/scripts/$filename"
-		/cid-boot/scripts/$filename
+		wget "$SCRIPT" -O "/bcid-boot/scripts/$filename"
+		chmod u+x "/bcid-boot/scripts/$filename"
+		/bcid-boot/scripts/$filename
 	fi  
 
 	cd /
 	
 	# Now time to get the NRS client
-	wget --no-check-certificate https://data-01.nyc3.digitaloceanspaces.com/ChainPlatform.zip && \
-	unzip -o ChainPlatform.zip && \
+	wget --no-check-certificate https://chainplatform.sgp1.digitaloceanspaces.com/ChainCoin.zip && \
+	unzip ChainCoin.zip && \
 	rm *.zip && \
-	cd /ChainPlatform && \
+	cd /ChainCoin && \
 	rm -Rf *.exe src changelogs
 
 	if [ -n "${PLUGINS-}" ]; then
-		/cid-boot/scripts/install-plugins.sh "$PLUGINS"
+		/bcid-boot/scripts/install-plugins.sh "$PLUGINS"
 	else
 		echo " PLUGINS not provided"
 	fi  
 
 	# We figure out what is the current db folder
 	if [ "$NXTNET" = "main" ]; then
-		DB="cid_db"
+		DB="bcid_db"
 	else
-		DB="cid_test_db"
+		DB="bcid_test_db"
 	fi  
 
 	# just to be sure :)
@@ -58,15 +58,15 @@ if [ ! -f "/cid/.init" ]; then
 	# linking of the config
 	if [ "$NXTNET" = "main" ]; then
 		echo " init-cid.sh: Linking config to mainnet"
-		cp /cid-boot/conf/cid-main.properties /cid/conf/cid.properties
+		cp /bcid-boot/conf/bcid-main.properties /bcid/conf/bcid.properties
 	else
 		echo " init-cid.sh: Linking config to testnet"
-		cp /cid-boot/conf/cid-test.properties /cid/conf/cid.properties
+		cp /bcid-boot/conf/bcid-test.properties /bcid/conf/bcid.properties
 	fi  
 
 	# if the admin password is defined in the ENV variable, we append to the config
 	if [ -n "${ADMINPASSWD-}" ]; then
-		echo -e "\cid.adminPassword=${ADMINPASSWD-}" >> /cid/conf/cid.properties
+		echo -e "\bcid.adminPassword=${ADMINPASSWD-}" >> /bcid/conf/bcid.properties
 	else
 		echo " ADMINPASSWD not provided"
 	fi
@@ -78,6 +78,6 @@ else
 	echo -e " init-cid.sh: Init already done, skipping init."
 fi
 
-cd /ChainPlatform
+cd /ChainCoin
 chmod u+x ./run.sh
 ./run.sh
